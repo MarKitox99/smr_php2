fa2cc10403967158aebf58d64e934f28d18c2d95
marcos <mvillanuevacampo@gmail.com>
2024-10-03 11:16:52 +0200

    Error de usuario incorrecto

--- index.php
+++ index.php
@@ -33,6 +33,9 @@ error_reporting(E_ALL);
 		padding: 10px;
 		line-height: 2em;
 	}
+	.error{
+			background-color: pink;
+	}
 </style>
 <title>Login</title>
 </head>
@@ -42,24 +45,26 @@ error_reporting(E_ALL);
 
 <?php
 if(!isset($_SESSION['id_user'])){
-	if(isset($_GET['password']) and $_GET['password']='1234' and $_POST == 'Usuario' ){
-		$_SESSION['id_user']=1;
+	if(isset($_POST['password'])){
+			if($_POST['password']='1234' and $_POST['Usuario']=='mvillanuevac@educantabria.es' ){
+				$_SESSION['id_user']=1;
+			}
+			else echo '<div class="error">Contraseña incorrecta</div>';
 	}
+}
 
-	else{
-			echo'<form>'
+
+if(!isset($_SESSION['id_user'])){
+			echo'<form method="post" class=formulario>'
 				.'<input name="Usuario" type="email" placeholder="Usuario"/>'
 				.'<input name="password" type="password" placeholder="contraseña"/>'
-				.'<button>Envia usuario y contraseña</button>
-				.</form>';
-	}				
+				.'<button>Envia usuario y contraseña</button>'
+				.'</form>';
 }
 if(isset($_SESSION['id_user'])){
 	echo '<div><img src="IMG_0911.jpg">';
 	echo '<a href="?logout=1">Cerrar sesión</a>';
 }	
-
-
 
 ?>
 
